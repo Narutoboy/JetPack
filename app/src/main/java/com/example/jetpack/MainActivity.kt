f3a801a0019f0175814d2bde66a5bb95742c9d12
Narutoboy <akshay.sharma21111995@gmail.com>
2024-04-20 14:19:04 +0530

    JetPack | Compose and state

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack.ui.theme.JetPackTheme
+
 /**
  * Jetpack Compose Native UI toolkit written in kotlin
  * Not part of framework,it is a jetpack Library
@@ -46,10 +59,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetPackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
 
                 }
             }
@@ -58,29 +68,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun sayHello(name:String ,modifier: Modifier = Modifier) {
+fun textCompose(name: String, modifier: Modifier = Modifier) {
     Surface {
-        Text(text ="Yeh Hello $name")
+        Text(
+            text = "Yeh Hello $name",
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center
+        )
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    JetPackTheme {
-        Greeting("Android")
-    }
+    // imageCompose()
+    //textCompose(name = "Akshay")
+    //buttonCompose("Click Me")
+    textFieldCompose()
 }
-@Preview(showBackground = true, showSystemUi = true, heightDp = 200)
+
 @Composable
-fun sayHelloPreview(){
-    sayHello(name = "Akshay")
+fun textFieldCompose() {
+    val state = remember { mutableStateOf("") }
+    TextField(value = state.value,
+        onValueChange = {state.value = it},
+        label = { Text(text = "Label text") })
+}
+
+@Composable
+private fun imageCompose() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "Main Icon",
+        colorFilter = ColorFilter.tint(Color.Red),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun buttonCompose(title: String) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = title)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Click me"
+        )
+    }
 }
