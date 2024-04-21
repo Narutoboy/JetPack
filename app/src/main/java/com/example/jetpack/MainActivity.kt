9811fb3b4f033441d0175306ac8b0982b365cf41
Narutoboy <akshay.sharma21111995@gmail.com>
2024-04-21 07:31:22 +0530

    JetPack | Compose Layout Column, Row, Box

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -25,6 +31,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack.ui.theme.JetPackTheme
 
 /**
@@ -86,14 +94,65 @@ fun GreetingPreview() {
     // imageCompose()
     //textCompose(name = "Akshay")
     //buttonCompose("Click Me")
-    textFieldCompose()
+    //textFieldCompose()
+    layout()
+}
+
+@Composable
+fun layout() {
+    /** For layout : Column, Row, Box
+     * Column :  Vertical arrangement
+     * Row : Horizontal arrangement */
+    //columnExample()
+    //rowExample()
+    //boxExample()
+    listViewItemExample(imgId = R.drawable.ic_launcher_foreground, name = "Akshay", occupation = "Mobile Genius")
+}
+@Composable
+fun listViewItemExample(imgId:Int,name:String,occupation:String) {
+Row(Modifier.padding(8.dp)) {
+    Image(painter = painterResource(id = imgId),
+        contentDescription = "Image",
+        modifier = Modifier.size(40.dp),
+    )
+}
+    Column {
+        Text(text = name, fontWeight =FontWeight.ExtraBold)
+        Text(text = occupation, fontWeight = FontWeight.Thin, fontSize = 24.sp)
+    }
+}
+
+@Composable
+fun boxExample() {
+    //Box are similar to frameLayout
+    Box(contentAlignment = Alignment.TopEnd) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "background")
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "foreground")
+    }
+}
+
+@Composable
+fun rowExample() {
+    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "C", fontSize = 24.sp)
+        Text(text = "D", fontSize = 24.sp)
+    }
+}
+
+@Composable
+fun columnExample() {
+    /*Arragement : how space will be divided between child element*/
+    Column(verticalArrangement = Arrangement.SpaceBetween) {
+        Text(text = "A", fontSize = 24.sp)
+        Text(text = "B", fontSize = 24.sp)
+    }
 }
 
 @Composable
 fun textFieldCompose() {
     val state = remember { mutableStateOf("") }
     TextField(value = state.value,
-        onValueChange = {state.value = it},
+        onValueChange = { state.value = it },
         label = { Text(text = "Label text") })
 }
 
