bda232bf540279f7c6c5dd16090c7796e8845658
Narutoboy <akshay.sharma21111995@gmail.com>
2024-04-24 07:43:45 +0530

    JetPack | Compose UI list & Details

---
+++ app/src/main/java/com/example/jetpack/QuoteListItem.kt
@@ -0,0 +1,111 @@
+package com.example.jetpack
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun QuoteListItem() {
+    Card(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Image(
+                imageVector = Icons.Filled.Close, contentDescription = "",
+                alignment = Alignment.TopStart,
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(100F)
+                    .background(Color.Black)
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "This is most valuable thing a man can spend",
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(.4f)
+                        .background(Color.Gray)
+                        .height(1.dp)
+                )
+                Text(
+                    text = "Theophrastus",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun QuoteDetails() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFffffff)
+                    )
+                )
+            )
+    ) {
+        Card(modifier = Modifier.padding(32.dp)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.List,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(45f)
+                )
+                Text(
+                    text = "Time is the most valuable thing one can spend",
+                    fontFamily = FontFamily.Cursive
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "akshay", style = MaterialTheme.typography.displayMedium)
+
+            }
+        }
+
+    }
+}
