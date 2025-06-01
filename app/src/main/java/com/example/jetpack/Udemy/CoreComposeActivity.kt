5dc79ec86f9e956ffb9afc4dbc7b24dad665843d
akshaysharma <erakshaysharma21@gmail.com>
2025-06-01 17:35:08 +0530

    JetPackLearn | Surface

---
+++ app/src/main/java/com/example/jetpack/Udemy/CoreComposeActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jetpack.Udemy
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+/*Surface: like CardView/ LinearLayout, surface is container used for background color, elevation, grouping*/
+class CoreComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            MainScreen()
+        }
+    }
+
+    @Composable
+    fun MainScreen() {
+        Surface(color = Color.Yellow, modifier = Modifier.fillMaxSize()) {
+            Surface(color = Color.Green, modifier = Modifier.wrapContentSize()) {
+                Text(
+                    text = "Hello Dev",
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+
+        }
+
+    }
+
+    @Preview
+    @Composable
+    fun MainScreenPreview(modifier: Modifier = Modifier) {
+        MainScreen()
+    }
+
+}
