c4b5906377047d4d6973ee24035ad9324da0efdb
akshaysharma <erakshaysharma21@gmail.com>
2025-06-01 19:06:02 +0530

    JetPackLearn | Row & Extracted in other composable

--- app/src/main/java/com/example/jetpack/Udemy/CoreComposeActivity.kt
+++ app/src/main/java/com/example/jetpack/Udemy/CoreComposeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.jetpack.Udemy
 
+import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,9 +19,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**Surface: like CardView/ LinearLayout, surface is container used for background color, elevation, grouping
  * [wrapContentSize] by default Center Align,we can change with Alignment.CenterStart
+ *
+ * Column : to arrange in vertical manner
+ * Row : to arrange in horizontal manner
+ * Box : to arrange in any order/ or in top of each other
+ *
  * */
 class CoreComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
@@ -30,15 +40,29 @@ class CoreComposeActivity : ComponentActivity() {
     @Composable
     fun MainScreen() {
         Surface(color = Color.Yellow, modifier = Modifier.fillMaxSize()) {
-            Surface(color = Color.Green, modifier = Modifier.wrapContentSize(align = Alignment.CenterStart)) {
-                Text(
-                    text = "Hello Dev",
-                    style = MaterialTheme.typography.displayLarge
-                )
+            Row {
+                HorizonalColoredBox()
+                HorizonalColoredBox(color=Color.Green)
+                HorizonalColoredBox(color=Color.Red)
+
             }
+
 
         }
 
+    }
+
+    @Composable
+    private fun HorizonalColoredBox(color:Color= Color.Blue) {
+        Surface(color = color, modifier = Modifier
+            .width(100.dp)
+            .height(600.dp)) {
+            Text(
+                text = "Hello ",
+                style = MaterialTheme.typography.displayLarge
+            )
+
+        }
     }
 
     @Preview
