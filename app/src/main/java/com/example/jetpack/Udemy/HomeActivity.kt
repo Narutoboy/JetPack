bbfe136b3fe6ace1ae074f2643189da714a53011
akshaysharma <erakshaysharma21@gmail.com>
2025-06-01 16:56:48 +0530

    JetPackLearn | Styling using Material or TextStyle

--- app/src/main/java/com/example/jetpack/Udemy/HomeActivity.kt
+++ app/src/main/java/com/example/jetpack/Udemy/HomeActivity.kt
@@ -8,9 +8,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -24,13 +28,15 @@ class HomeActivity : ComponentActivity() {
         }
     }
 }
-/*Modifier : Order of Modifier matter */
+/*Modifier : Order of Modifier matter
+*with Style we can used predefine style e.g Material theme and customize by using TextStyle */
 @Composable
 fun GreetingText(name:String) {
     Text(text = "Hello $name", modifier = Modifier.width(200.dp)
         .height(240.dp)
         .clickable {  }
-        .padding(20.dp))
+        .padding(20.dp),
+        style = MaterialTheme.typography.bodyLarge)
 }
 
 
