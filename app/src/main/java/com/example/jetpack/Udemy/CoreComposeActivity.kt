c8671ea1cf7a87df36b2d17c2f3deaa1533d0123
akshaysharma <erakshaysharma21@gmail.com>
2025-06-01 17:40:09 +0530

    JetPackLearn | Surface alignment

--- app/src/main/java/com/example/jetpack/Udemy/CoreComposeActivity.kt
+++ app/src/main/java/com/example/jetpack/Udemy/CoreComposeActivity.kt
@@ -11,11 +11,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-/*Surface: like CardView/ LinearLayout, surface is container used for background color, elevation, grouping*/
+/**Surface: like CardView/ LinearLayout, surface is container used for background color, elevation, grouping
+ * [wrapContentSize] by default Center Align,we can change with Alignment.CenterStart
+ * */
 class CoreComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
@@ -27,7 +30,7 @@ class CoreComposeActivity : ComponentActivity() {
     @Composable
     fun MainScreen() {
         Surface(color = Color.Yellow, modifier = Modifier.fillMaxSize()) {
-            Surface(color = Color.Green, modifier = Modifier.wrapContentSize()) {
+            Surface(color = Color.Green, modifier = Modifier.wrapContentSize(align = Alignment.CenterStart)) {
                 Text(
                     text = "Hello Dev",
                     style = MaterialTheme.typography.displayLarge
